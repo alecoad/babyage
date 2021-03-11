858050a9a2b8aa2837787e74a1a8ba632a66b3d7
alecoad <andrewlecoad@gmail.com>
2021-03-10 20:57:40 -0700

    add chronological and adjusted month calculation

--- babyage.py
+++ babyage.py
@@ -11,13 +11,21 @@ day_age = today - birth
 week_age = math.floor(day_age.days / 7)
 weekday_remainder = day_age.days % 7
 
+# Calculate chronological age in months (1 month ~ 30.417 days)
+month_age = round(day_age.days / 30.417, 2)
+
 # Calculate adjusted age in weeks + days
 adjusted_days = today - due_date
 adjusted_weeks = math.floor(adjusted_days.days / 7)
 adjusted_remainder = adjusted_days.days % 7
 
+# Calculate adjusted age in months
+adjusted_months = round(adjusted_days.days / 30.417, 2)
+
 # Print 'age report'
 print("\nADELINE & JAMES AGE REPORT:\n")
 print(f" * Adeline and James are {week_age} weeks and {weekday_remainder} day(s) old.")
-print(f"   - This amounts to {day_age.days} total days.\n")
-print(f" * Their adjusted age is {adjusted_weeks} weeks and {adjusted_remainder} day(s).\n")
+print(f"   - This amounts to {day_age.days} total days.")
+print(f"   - This is equivalent to {month_age} months.\n")
+print(f" * Their adjusted age is {adjusted_weeks} weeks and {adjusted_remainder} day(s).")
+print(f"   - This is equivalent to {adjusted_months} months.\n")
